13abbef725614103c355e987e549fddcd8ea96e1
Steve <admin@cryptostat.org>
2019-05-06 10:15:19 +1000

    change docs version to 1.0 instead of 1.1

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -24,9 +24,9 @@ copyright = '2019, Ledgerium Pty Ltd.'
 author = 'Ledgerium Pty Ltd.'
 
 # The short X.Y version
-version = '1.1'
+version = '1.0'
 # The full version, including alpha/beta/rc tags
-release = '1.1.0alpha'
+release = '1.0.0alpha'
 
 
 # -- General configuration ---------------------------------------------------
